31974af1047802c5a3fa588ac8cfa65d5c50b691
Muaaz Butt <muaazbutt585@gmail.com>
2024-07-28 16:06:34 +0500

    Inheritance in OOP

---
+++ Inheritance.py
@@ -0,0 +1,21 @@
+class Employee:
+  def __init__(self) :
+    print("Constructor of Employee")
+  a = 1
+  
+class Programmer(Employee):
+  def __init__(self) :
+    super().__init__()
+    print("Constructor of programmer")  
+  b = 2
+  
+class Manager(Programmer):
+  def __init__(self):
+    super().__init__()
+    print("Constructor of manager")
+  c = 3
+    
+  
+o = Manager()
+print(o.a, o.b, o.c)
+    
